8a172492f1b4740290b93e8c7d68a58e62216965
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-03-07 22:45:02 -0300

    footer layout mobile

--- js/home.js
+++ js/home.js
@@ -1,10 +1,22 @@
 let input = document.querySelector('.menu__search-input');
+let buttonLogin = document.querySelector('.menu__button');
+let menuSearch = document.querySelector('.menu__search');
+
 input.addEventListener('focus', (event) => {
-   event.target.style.borderBottom = "1px solid #00C6CC";
+   event.target.style.borderBottom = "1px solid #5595E9";
+   if(window.innerWidth < 768) { 
+    input.style.borderBottom = 'none';
+   }
 });
 
 input.addEventListener('blur', (event) => {
    event.target.style.borderBottom = "none";
+   ocultaBarraDePesquisa();
+   setTimeout(() => {
+        input.value = '';
+        input.placeholder = 'O que deseja encontrar?';
+   }, 4000)
+   
 });
 
 const searchIcon = document.querySelector('.menu__search-icon')
@@ -16,5 +28,29 @@ searchIcon.addEventListener('mouseover', function() {
 
 searchIcon.addEventListener('mouseout', function() {
     path.style.fill = '#A2A2A2';
-  });
-  
+});
+
+
+function mostraBarraDePesquisa() {
+    if(window.innerWidth < 768) {
+        input.style.display = 'flex';
+        menuSearch.style.display = 'flex';
+        buttonLogin.style.display = 'none';
+        searchIcon.style.marginTop = '6px';
+        searchIcon.style.marginLeft = '6px';
+        input.classList.add('menu__search-input--mobile');
+    } 
+}
+
+function ocultaBarraDePesquisa() {
+    if(window.innerWidth < 768) {
+        input.style.display = 'none';
+        buttonLogin.style.display = 'block';
+        input.classList.remove('menu__search-input--mobile');
+    }
+}
+
+searchIcon.addEventListener('click', () => {
+    mostraBarraDePesquisa();
+});
+
