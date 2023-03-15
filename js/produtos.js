84c3a677f775acffd800ec9506414c8571598a07
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-03-15 16:35:24 -0300

    ajustes nos ícones de editar e excluir tablet

--- js/produtos.js
+++ js/produtos.js
@@ -84,3 +84,119 @@ const contatoForm = document.querySelector('.contato__form');
 contatoForm.addEventListener('submit', (event) => {
     event.preventDefault();
 })
+
+const produtosBabyYoda = document.querySelector('.produtos__baby-yoda');
+const iconeEditarBabyYoda = produtosBabyYoda.children[1];
+const iconeExcluirBabyYoda = produtosBabyYoda.children[2];
+
+const produtosNintendo = document.querySelector('.produtos__nintendo');
+const iconeEditarNintendo = produtosNintendo.children[1];
+const iconeExcluirNintendo = produtosNintendo.children[2];
+
+const produtosJoystick = document.querySelector('.produtos__joystick');
+const iconeEditarJoystick = produtosJoystick.children[1];
+const iconeExcluirJoystick = produtosJoystick.children[2];
+
+const produtosPlayStation = document.querySelector('.produtos__joystick');
+const iconeEditarPlayStation = produtosPlayStation.children[1];
+const iconeExcluirPlayStation = produtosPlayStation.children[2];
+
+const produtosOculos = document.querySelector('.produtos__oculos');
+const iconeEditarOculos = produtosOculos.children[1];
+const iconeExcluirOculos = produtosOculos.children[2];
+
+const produtosGameBoy = document.querySelector('.produtos__game-boy');
+const iconeEditarGameBoy = produtosGameBoy.children[1];
+const iconeExcluirGameBoy = produtosGameBoy.children[2];
+
+const produtosNintendoSwitch = document.querySelector('.produtos__nintendo-switch');
+const iconeEditarNintendoSwitch = produtosNintendoSwitch.children[1];
+const iconeExcluirNintendoSwitch = produtosNintendoSwitch.children[2];
+
+function adicionarOuRemoverClasses() {
+  if (window.innerWidth >= 768 && window.innerWidth <= 993) {
+    iconeEditarBabyYoda.classList.remove('produtos__icone-editar5');
+    iconeEditarBabyYoda.classList.add('produtos__icone-editar1--tablet');
+    iconeExcluirBabyYoda.classList.remove('produtos__icone-excluir5');
+    iconeExcluirBabyYoda.classList.add('produtos__icone-excluir1--tablet');
+
+    iconeEditarNintendo.classList.remove('produtos__icone-editar3');
+    iconeEditarNintendo.classList.add('produtos__icone-editar1--tablet');
+    iconeExcluirNintendo.classList.remove('produtos__icone-excluir3');
+    iconeExcluirNintendo.classList.add('produtos__icone-excluir1--tablet');
+
+    iconeEditarOculos.classList.remove('produtos__icone-editar5');
+    iconeEditarOculos.classList.add('produtos__icone-editar1--tablet');
+    iconeExcluirOculos.classList.remove('produtos__icone-excluir5');
+    iconeExcluirOculos.classList.add('produtos__icone-excluir1--tablet');
+
+    iconeEditarGameBoy.classList.remove('produtos__icone-editar6');
+    iconeEditarGameBoy.classList.add('produtos__icone-editar4--tablet');
+    iconeExcluirGameBoy.classList.remove('produtos__icone-excluir6');
+    iconeExcluirGameBoy.classList.add('produtos__icone-excluir4--tablet');
+
+    iconeEditarJoystick.classList.remove('produtos__icone-editar1');
+    iconeEditarJoystick.classList.add('produtos__icone-editar3--tablet');
+    iconeExcluirJoystick.classList.remove('produtos__icone-excluir1');
+    iconeExcluirJoystick.classList.add('produtos__icone-excluir3--tablet');
+    
+    iconeEditarPlayStation.classList.remove('produtos__icone-editar2');
+    iconeEditarPlayStation.classList.add('produtos__icone-editar4--tablet');
+    iconeExcluirPlayStation.classList.remove('produtos__icone-excluir2');
+    iconeExcluirPlayStation.classList.add('produtos__icone-excluir4--tablet');
+
+    iconeEditarNintendoSwitch.classList.remove('produtos__icone-editar4');
+    iconeEditarNintendoSwitch.classList.add('produtos__icone-editar5--tablet');
+    iconeExcluirNintendoSwitch.classList.remove('produtos__icone-excluir4');
+    iconeExcluirNintendoSwitch.classList.add('produtos__icone-excluir5--tablet');
+
+  } else {
+    iconeEditarBabyYoda.classList.remove('produtos__icone-editar1--tablet');
+    iconeEditarBabyYoda.classList.add('produtos__icone-editar5');
+    iconeExcluirBabyYoda.classList.remove('produtos__icone-excluir1--tablet');
+    iconeExcluirBabyYoda.classList.add('produtos__icone-excluir5');
+
+    iconeEditarNintendo.classList.remove('produtos__icone-editar1--tablet');
+    iconeEditarNintendo.classList.add('produtos__icone-editar3');
+    iconeExcluirNintendo.classList.remove('produtos__icone-excluir1--tablet');
+    iconeExcluirNintendo.classList.add('produtos__icone-excluir3');
+
+    iconeEditarOculos.classList.remove('produtos__icone-editar1--tablet');
+    iconeEditarOculos.classList.add('produtos__icone-editar5');
+    iconeExcluirOculos.classList.remove('produtos__icone-excluir1--tablet');
+    iconeExcluirOculos.classList.add('produtos__icone-excluir5');
+
+    iconeEditarGameBoy.classList.remove('produtos__icone-editar4--tablet');
+    iconeEditarGameBoy.classList.add('produtos__icone-editar6');
+    iconeExcluirGameBoy.classList.remove('produtos__icone-excluir4--tablet');
+    iconeExcluirGameBoy.classList.add('produtos__icone-excluir6');
+
+    iconeEditarJoystick.classList.remove('produtos__icone-editar3--tablet');
+    iconeEditarJoystick.classList.add('produtos__icone-editar1');
+    iconeExcluirJoystick.classList.remove('produtos__icone-excluir3--tablet');
+    iconeExcluirJoystick.classList.add('produtos__icone-excluir1');
+
+    iconeEditarPlayStation.classList.remove('produtos__icone-editar4--tablet');
+    iconeEditarPlayStation.classList.add('produtos__icone-editar2');
+    iconeExcluirPlayStation.classList.remove('produtos__icone-excluir4--tablet');
+    iconeExcluirPlayStation.classList.add('produtos__icone-excluir2');
+
+    iconeEditarNintendoSwitch.classList.remove('produtos__icone-editar5--tablet');
+    iconeEditarNintendoSwitch.classList.add('produtos__icone-editar4');
+    iconeExcluirNintendoSwitch.classList.remove('produtos__icone-excluir5--tablet');
+    iconeExcluirNintendoSwitch.classList.add('produtos__icone-excluir4');
+
+  }
+}
+
+// chamando a função quando a tela é redimensionada
+window.addEventListener('resize', adicionarOuRemoverClasses);
+
+// chamando a função na carga inicial da página
+adicionarOuRemoverClasses();
+
+
+
+
+
+
